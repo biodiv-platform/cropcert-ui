6ef5947a0d848d30da5bfe743bf7eca06ed07e70
Harsh Zalavadiya <harsh@strandls.com>
2019-09-25 15:54:21 +0530

    :bug: blank data retrival for edit

--- src/pages/page/manage.tsx
+++ src/pages/page/manage.tsx
@@ -8,14 +8,22 @@ const ManagePagePage = ({ query }) => {
   const [page, setPage] = useState({ success: false, data: {} });
 
   useEffect(() => {
-    if (query.id) {
+    if (query.id > 0) {
       axGetPageByPageId(query.id).then(data => setPage(data));
+    } else {
+      setPage({ ...page, success: true });
     }
   }, [query]);
 
   return (
     <Container>
-      <ManagePage {...query} id={query.id || query.parentId} page={page.data} />
+      {page.success && (
+        <ManagePage
+          {...query}
+          id={query.id || query.parentId}
+          page={page.data}
+        />
+      )}
     </Container>
   );
 };

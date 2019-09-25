c59d8a7b62304b39bc7adfc4a75c2e2d650a1e00
Harsh Zalavadiya <harsh@strandls.com>
2019-09-25 15:46:41 +0530

    :recycle: migration of inputs to react components

--- src/components/@core/formik/index.tsx
+++ src/components/@core/formik/index.tsx
@@ -1,27 +1,23 @@
 import { formattedTimeStamp, local2utc, utc2local } from "@utils/basic.util";
+import { Dropdown, TextInput } from "carbon-components-react";
 import React, { useEffect, useState } from "react";
 import DateTimePicker from "react-datetime-picker";
 
 export const textInput = ({ field, form: { errors }, label, ...props }) => {
   return (
-    <fieldset className="bx--fieldset">
-      <div className="bx--form-item">
-        <label className="bx--label">{label}</label>
-        <input
-          type="text"
-          id={field.name}
-          autoComplete="off"
-          className="bx--text-input"
-          placeholder={`Enter ${label}`}
-          {...field}
-          {...props}
-          {...(errors[field.name] && { "data-invalid": true })}
-        />
-        {errors[field.name] && (
-          <div className="bx--form-requirement">{errors[field.name]}</div>
-        )}
-      </div>
-    </fieldset>
+    <TextInput
+      type="text"
+      id={field.name}
+      autoComplete="off"
+      placeholder={`Enter ${label}`}
+      labelText={field.name}
+      {...field}
+      {...props}
+      {...(errors[field.name] && {
+        invalid: true,
+        invalidText: errors[field.name],
+      })}
+    />
   );
 };
 
@@ -48,114 +44,57 @@ export const dateTimeInput = ({
     : new Date();
 
   return (
-    <fieldset className="bx--fieldset">
-      <div className="bx--form-item">
-        {hint ? (
-          <div className="bx--form__helper-text">
-            Provide date and time between
-            <br />
-            {minDate && formattedTimeStamp(minDate)}
-            {minDate && maxDate && " - "}
-            {maxDate && formattedTimeStamp(maxDate)}
-          </div>
-        ) : (
-          <label className="bx--label">{label}</label>
-        )}
-        <DateTimePicker
-          className="bx--text-input"
-          onChange={setDateTimeValue}
-          value={dateTimeValue}
-          autoComplete={false}
-          format="dd-MM-yyyy H:mm"
-          required={true}
-          disabled={disabled}
-          minDate={minDate}
-          maxDate={maxDate}
-        />
-        {hasErrors && (
-          <div className="bx--form-requirement">{errors[field.name]}</div>
-        )}
-      </div>
-    </fieldset>
-  );
-};
-
-export const textAreaInput = ({
-  field,
-  form: { touched, errors },
-  label,
-  ...props
-}) => {
-  const hasErrors = touched[field.name] && errors[field.name];
-  return (
-    <fieldset className="bx--fieldset">
-      <div className="bx--form-item">
-        <label className="bx--label">{label}</label>
-        <div className="bx--text-area__wrapper" style={{ width: "100%" }}>
-          <textarea
-            placeholder={`Enter ${label}`}
-            className="bx--text-area"
-            id={field.name}
-            style={{ width: "100%" }}
-            {...field}
-            {...props}
-            {...(hasErrors ? { "data-invalid": true } : {})}
-          />
-          {hasErrors && (
-            <div className="bx--form-requirement">{errors[field.name]}</div>
-          )}
+    <div className="bx--form-item">
+      {hint ? (
+        <div className="bx--form__helper-text">
+          Provide date and time between
+          <br />
+          {minDate && formattedTimeStamp(minDate)}
+          {minDate && maxDate && " - "}
+          {maxDate && formattedTimeStamp(maxDate)}
         </div>
-      </div>
-    </fieldset>
+      ) : (
+        <label className="bx--label">{label}</label>
+      )}
+      <DateTimePicker
+        className="bx--text-input"
+        onChange={setDateTimeValue}
+        value={dateTimeValue}
+        autoComplete={false}
+        format="dd-MM-yyyy H:mm"
+        required={true}
+        disabled={disabled}
+        minDate={minDate}
+        maxDate={maxDate}
+      />
+      {hasErrors && (
+        <div className="bx--form-requirement">{errors[field.name]}</div>
+      )}
+    </div>
   );
 };
 
 export const selectInput = ({
   field,
-  form: { touched, errors },
+  form: { touched, errors, setFieldValue },
   label,
   options,
   ...props
 }) => {
   const hasErrors = touched[field.name] && errors[field.name];
   return (
-    <fieldset className="bx--fieldset">
-      <div className="bx--form-item">
-        <div className="bx--select eco--w100">
-          <label className="bx--label">{label}</label>
-          <div className="bx--select-input__wrapper">
-            <select
-              id={field.name}
-              className="bx--select-input eco--w100"
-              {...field}
-              {...props}
-              {...(hasErrors ? { "data-invalid": true } : {})}
-            >
-              {options.map(o => (
-                <option key={o.value} value={o.value}>
-                  {o.label}
-                </option>
-              ))}
-            </select>
-            <svg
-              focusable="false"
-              preserveAspectRatio="xMidYMid meet"
-              style={{ willChange: "transform" }}
-              xmlns="http://www.w3.org/2000/svg"
-              className="bx--select__arrow"
-              width="10"
-              height="6"
-              viewBox="0 0 10 6"
-              aria-hidden="true"
-            >
-              <path d="M5 6L0 1 .7.3 5 4.6 9.3.3l.7.7z" />
-            </svg>
-          </div>
-          {hasErrors && (
-            <div className="bx--form-requirement">{errors[field.name]}</div>
-          )}
-        </div>
-      </div>
-    </fieldset>
+    <Dropdown
+      id={field.name}
+      items={options}
+      label={`Select ${field.name}`}
+      titleText={field.name}
+      ariaLabel={field.name}
+      disabled={options.length < 1}
+      onChange={e => {
+        setFieldValue(field.name, e.selectedItem.value);
+      }}
+      {...props}
+      {...(hasErrors && { invalid: true, invalidText: errors[field.name] })}
+    />
   );
 };

5fe1080d9be8519b5ce48b9ea1ab21e3e5e5fba3
Harsh Zalavadiya <harsh@strandls.com>
2019-09-26 20:35:50 +0530

    ✨ added lists into TinyMCE input

--- src/components/@core/wysiwyg/index.tsx
+++ src/components/@core/wysiwyg/index.tsx
@@ -2,9 +2,10 @@ import "tinymce/tinymce";
 import "tinymce/plugins/code";
 import "tinymce/plugins/image";
 import "tinymce/plugins/link";
+import "tinymce/plugins/lists";
 import "tinymce/plugins/table";
 import "tinymce/themes/silver/theme";
-import "./index.scss"
+import "./index.scss";
 
 import { axUploadHandler } from "@services/pages.services";
 import { Editor } from "@tinymce/tinymce-react";
@@ -19,9 +20,9 @@ export default function TinymceEditor({ data, onUpdate }) {
         width: "100%",
         height: "400px",
         relative_urls: false,
-        plugins: ["link", "image", "table", "code"],
+        plugins: ["link", "image", "table", "code", "lists"],
         toolbar:
-          "undo redo | bold italic | alignleft aligncenter alignright alignjustify | link image table | code",
+          "undo redo | bold italic numlist bullist | alignleft aligncenter alignright alignjustify | link image table | code",
         images_upload_handler: axUploadHandler,
         images_upload_base_path: "/",
       }}

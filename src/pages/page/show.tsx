bff2ba1179a85188bf8b40250c17c0681653af87
Harsh Zalavadiya <harsh@strandls.com>
2019-10-08 15:01:18 +0530

    :sparkles: show create and edit on page show

--- src/pages/page/show.tsx
+++ src/pages/page/show.tsx
@@ -1,8 +1,11 @@
 import "@styles/medium.scss";
 
+import { Add16, Edit16 } from "@carbon/icons-react";
 import Container from "@components/@core/container";
 import withLocation from "@components/withLocation";
 import { axGetPageByPageId } from "@services/pages.services";
+import { Button } from "carbon-components-react";
+import { Link } from "gatsby";
 import React, { useEffect, useState } from "react";
 
 const ShowPage = ({ query }) => {
@@ -18,6 +21,23 @@ const ShowPage = ({ query }) => {
     <Container>
       {page.success && (
         <div className="blog">
+          <div className="float-right">
+            <Button
+              as={Link}
+              className="mr-2"
+              renderIcon={Add16}
+              to={`/page/list`}
+            >
+              Create
+            </Button>
+            <Button
+              as={Link}
+              renderIcon={Edit16}
+              to={`/page/manage?id=${query.id}&mode=edit`}
+            >
+              Edit
+            </Button>
+          </div>
           <h1>{page.data.heading}</h1>
           <div dangerouslySetInnerHTML={{ __html: page.data.content }} />
         </div>

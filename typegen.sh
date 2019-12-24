ae2646ff44673fda73bdac931e5204cc958b46f0
Harsh Zalavadiya <harsh@strandls.com>
2019-12-24 19:37:58 +0530

    :recycle: removed endpoint postfix

--- typegen.sh
+++ typegen.sh
@@ -6,7 +6,7 @@ microservices=( "cropcert" "user" "pages" "traceability" )
 
 for m in "${microservices[@]}"
 do
-    wget -O "$m.json" "http://$endpoint/$m-api/api/swagger.json"
+    wget -O "$m.json" "http://$endpoint/$m/api/swagger.json"
     npx sw2dts "$m.json" > "types/$m.d.ts"
     rm "$m.json"
 done

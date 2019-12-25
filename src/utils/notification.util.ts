8e37d562e9b5eb7cfb9f80dbee0c331771b6fb78
Harsh Zalavadiya <harsh@foobars.in>
2019-12-25 18:40:49 +0530

    :recycle: notification logic optimization

--- src/utils/notification.util.ts
+++ src/utils/notification.util.ts
@@ -4,34 +4,25 @@ import cogoToast from "cogo-toast";
 import { compiledMessage } from "./basic.util";
 
 export enum NotificationType {
-  Success,
-  Info,
-  Warning,
-  Error
+  Success = "success",
+  Info = "info",
+  Warning = "warn",
+  Error = "error"
 }
 
 const notification = (message, type = NotificationType.Error, variables = {}) => {
   const m = compiledMessage(message.toString(), variables);
-  const pos: any = { position: "bottom-center" };
   if (isBrowser) {
-    switch (type) {
-      case NotificationType.Error:
-        cogoToast.error(m, pos);
-        break;
-
-      case NotificationType.Info:
-        cogoToast.info(m, pos);
-        break;
-
-      case NotificationType.Warning:
-        cogoToast.warn(m, pos);
-        break;
-
-      default:
-        cogoToast.success(m, pos);
-        break;
-    }
+    const toaster = cogoToast[type];
+    const { hide } = toaster(m, {
+      position: "top-center",
+      hideAfter: 10,
+      onClick: () => {
+        hide();
+      }
+    });
   }
+  console.info(m);
 };
 
 export default notification;

962099441acecee2833eae3941437cffdff53ab6
harshzalavadiya <harsh@strandls.com>
2022-02-07 13:53:23 +0530

    fix: :bug: prevent tinymce force re-render

--- src/components/@core/formik/wysiwyg/index.tsx
+++ src/components/@core/formik/wysiwyg/index.tsx
@@ -1,7 +1,7 @@
 import { FormControl, FormErrorMessage, FormHelperText, FormLabel } from "@chakra-ui/react";
 import { useField } from "formik";
 import dynamic from "next/dynamic";
-import React from "react";
+import React, { useMemo } from "react";
 
 const Editor = dynamic(() => import("./wysiwyg"), { ssr: false });
 
@@ -10,10 +10,13 @@ const wysiwygInput = ({ name, label, hint = false, hintText = "", mb = 4 }) => {
 
   const onEditorValueChange = (v) => helpers.setValue(v);
 
+  // this prevents tinmce to avoid force re-render when initialvalue is changed
+  const initialValue = useMemo(() => field.value, []);
+
   return (
     <FormControl isInvalid={meta.touched && meta.error ? true : false} mb={mb}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Editor data={field.value} onUpdate={onEditorValueChange} />
+      <Editor data={initialValue} onUpdate={onEditorValueChange} />
       <FormErrorMessage>{meta.error}</FormErrorMessage>
       {hint && <FormHelperText>{hintText}</FormHelperText>}
     </FormControl>

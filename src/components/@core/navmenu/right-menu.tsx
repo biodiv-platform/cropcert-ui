62af8226141b35369a1767239f2478235f15fc91
Harsh Zalavadiya <harsh@strandls.com>
2022-08-12 17:05:05 +0530

    refactor: :recycle: shows user role

--- src/components/@core/navmenu/right-menu.tsx
+++ src/components/@core/navmenu/right-menu.tsx
@@ -1,11 +1,16 @@
 import { Link } from "@chakra-ui/react";
 import useGlobalState from "@hooks/use-global-store";
 import NextLink from "next/link";
-import React from "react";
+import React, { useMemo } from "react";
 import LogoutIcon from "src/icons/logout";
 
 function NavbarRightMenu() {
-  const { user, isLoggedIn } = useGlobalState();
+  const { user, authorizedRoles, isLoggedIn } = useGlobalState();
+
+  const visualRole = useMemo(
+    () => (isLoggedIn ? authorizedRoles?.[0].replace("_", " ").toLowerCase() : null),
+    [isLoggedIn, authorizedRoles]
+  );
 
   return (
     <ul className="main-menu right">
@@ -13,7 +18,7 @@ function NavbarRightMenu() {
         <li>
           <NextLink href="/auth/sign-out" passHref={true}>
             <Link>
-              {user.name} <LogoutIcon ml={1} />
+              {user.name} ({visualRole}) <LogoutIcon ml={1} />
             </Link>
           </NextLink>
         </li>

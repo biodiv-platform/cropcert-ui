74ad49bb4b5ee2d23ef0d2ac0f1d309cf1b138b2
Harsh Zalavadiya <harsh@strandls.com>
2022-09-06 19:03:06 +0530

    refactor: :recycle: show opengraph image

--- src/components/pages/page/show/index.tsx
+++ src/components/pages/page/show/index.tsx
@@ -1,4 +1,7 @@
+import { Box, Image } from "@chakra-ui/react";
 import Container from "@components/@core/container";
+import { RESOURCE_SIZE } from "@static/constants";
+import { getResourceThumbnail, RESOURCE_CTX } from "@utils/media";
 import React from "react";
 
 import { UsePagesProvider } from "../common/sidebar/use-pages-sidebar";
@@ -10,11 +13,20 @@ interface PageShowPageComponentProps {
 }
 
 export default function PageShowPageComponent({ page }: PageShowPageComponentProps) {
+  const ogImage = getResourceThumbnail(
+    RESOURCE_CTX.PAGES,
+    page.socialPreview || page.galleryData?.[0]?.fileName,
+    RESOURCE_SIZE.TWITTER
+  );
+
   return (
     <UsePagesProvider currentPage={page} linkType="show">
-      <PageHeader page={page} />
+      <PageHeader page={page} ogImage={ogImage} />
 
       <Container maxW="48rem" py={6}>
+        <Box mb={6}>
+          <Image h="150px" src={ogImage} alt={page.title} />
+        </Box>
         <Content html={page.content} />
       </Container>
     </UsePagesProvider>

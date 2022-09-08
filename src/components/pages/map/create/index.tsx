2a79d8c432bc13e1f269b29b0e098542faba86d7
Harsh Zalavadiya <harsh@strandls.com>
2022-09-08 14:34:57 +0530

    fix: :bug: passing token on upload

--- src/components/pages/map/create/index.tsx
+++ src/components/pages/map/create/index.tsx
@@ -1,9 +1,10 @@
 import { Box } from "@chakra-ui/react";
 import useGlobalState from "@hooks/use-global-state";
-import { ENDPOINT } from "@static/constants";
+import { ENDPOINT, TOKEN } from "@static/constants";
 import { useRouter } from "next/router";
 import useTranslation from "next-translate/useTranslation";
-import React from "react";
+import { parseCookies } from "nookies";
+import React, { useMemo } from "react";
 
 import { NakshaLayerUpload } from "./naksha-upload";
 
@@ -11,6 +12,11 @@ export default function MapCreatePageComponent() {
   const { user } = useGlobalState();
   const router = useRouter();
   const { lang } = useTranslation();
+
+  const accessToken = useMemo(() => {
+    const pk = parseCookies();
+    return pk[TOKEN.ACCESS];
+  }, [user]);
 
   const handleOnLayerUpload = (status) => {
     if (status) {
@@ -24,7 +30,7 @@ export default function MapCreatePageComponent() {
         lang={lang}
         nakshaEndpoint={`${ENDPOINT.NAKSHA}/layer/upload`}
         callback={handleOnLayerUpload}
-        bearerToken={`Bearer ${user.accessToken}`}
+        bearerToken={`Bearer ${accessToken}`}
       />
     </Box>
   );

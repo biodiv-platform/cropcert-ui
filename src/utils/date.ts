d36eec71272f6b8dd6ccec77b2c9750ddb3260b2
Harsh Zalavadiya <harsh@strandls.com>
2022-09-09 19:11:10 +0530

    fix: :bug: datepicker bugfix

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -13,14 +13,14 @@ const FORMAT_DATE_REVERSE = "YYYY-MM-DD";
 const FORMAT_DATE_READABLE_TIMESTAMP = "D MMMM YYYY";
 const FORMAT_EXIF_TIMESTAMP = "YYYY:MM:DD HH:mm:ss";
 
-export const dateToUTC = (ts?) => (ts ? dayjs(ts, FORMAT_DATE_TIMESTAMP).utc() : dayjs().utc());
+export const dateToUTC = (ts?) => (ts ? dayjs(ts).utc() : dayjs().utc());
 
 export const parseDate = (ts) => (ts ? dayjs(ts, FORMAT_DATE_TIMESTAMP).toDate() : new Date());
 export const formatDate = (ts, format = FORMAT_DATE_TIMESTAMP) => ts && dayjs(ts).format(format);
 export const parseDateReverse = (ts) => ts && dayjs(ts, FORMAT_DATE_REVERSE).toDate();
 export const formatDateReverse = (ts) => ts && dayjs(ts).format(FORMAT_DATE_REVERSE);
 export const formatDateFromUTC = (ts) => dayjs.utc(ts).local().format(FORMAT_DATE_TIMESTAMP);
-export const parseDateFromUTC = (ts) => dayjs.utc(ts).local().toDate();
+export const parseDateFromUTC = (ts) => ts && dayjs.utc(ts).local().toDate();
 
 export const parseDateRange = (ts) => (ts ? ts.map((i) => parseDate(i)) : []);
 export const formatDateRange = (ts) => (ts ? ts.map((i) => formatDate(i)) : []);

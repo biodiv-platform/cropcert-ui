820b1177bbaceb6f76ab400d331cfa5676161e58
logan1x <sharmakhushal78@gmail.com>
2023-06-15 22:29:17 +0530

    change: dashboard page static card entry

--- src/components/pages/dashboard/index.tsx
+++ src/components/pages/dashboard/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Link, Stat, StatHelpText, StatNumber, Heading } from "@chakra-ui/react";
+import NextLink from "next/link";
 import Container from "@components/@core/container";
 import CoreGrid from "@components/@core/layout/grid";
 import useGlobalState from "@hooks/use-global-state";
@@ -21,18 +22,49 @@ export default function DashboardPageComponent() {
 
   const renderCardGroup = (cardGroup, index) => {
     const linkList = getLinks(cardGroup.children);
-    return linkList.length > 0 ? (
-      <React.Fragment key={index}>
-        <Heading mb={4} size="lg" style={{ fontSize: "1.8rem" }}>
-          {cardGroup.title}
-        </Heading>
-        <CoreGrid mb={6}>{linkList}</CoreGrid>
-      </React.Fragment>
-    ) : null;
+    return (
+      cardGroup.title !== "Traceability" &&
+      (linkList.length > 0 ? (
+        <React.Fragment key={index}>
+          <Heading mb={4} size="lg" style={{ fontSize: "1.8rem" }}>
+            {cardGroup.title}
+          </Heading>
+          <CoreGrid mb={6}>{linkList}</CoreGrid>
+        </React.Fragment>
+      ) : null)
+    );
   };
 
   return (
     <Container pt={6}>
+      <Box>
+        <Heading mb={4} size="lg" style={{ fontSize: "1.8rem" }}>
+          {"Traceability"}
+        </Heading>
+        <CoreGrid mb={6}>
+          <Box
+            w="100%"
+            rounded="lg"
+            bg="gray.100"
+            _hover={{
+              bg: "blue.100",
+            }}
+            px={6}
+            py={5}
+          >
+            <Link href={"/traceability/traceability-workflow"} style={{ textDecoration: "none" }}>
+              <Stat>
+                <StatNumber lineHeight="1em" mb={4} fontSize="2xl">
+                  {"Traceability Workflow"}
+                </StatNumber>
+                <StatHelpText fontSize="md" mb={0}>
+                  {"Traceability Workflow"} &rarr;
+                </StatHelpText>
+              </Stat>
+            </Link>
+          </Box>
+        </CoreGrid>
+      </Box>
       <Box maxWidth="60rem">{links.map(renderCardGroup)}</Box>
     </Container>
   );

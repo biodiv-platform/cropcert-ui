467125dfc221175bc784eced728588c47cf4cad3
logan1x <sharmakhushal78@gmail.com>
2023-06-15 14:43:58 +0530

    fix: lot show page from batch not working

--- src/components/@core/table/lot-cell.tsx
+++ src/components/@core/table/lot-cell.tsx
@@ -3,12 +3,23 @@ import { Link, Tooltip } from "@chakra-ui/react";
 import NextLink from "next/link";
 import React from "react";
 
-export default function LotCell({ lotId, _id }: { lotId?; _id? }) {
+export default function LotCell({
+  lotId,
+  _id,
+  lotIdMongo,
+  batchStatus,
+}: {
+  lotId?;
+  _id?;
+  lotIdMongo?;
+  batchStatus?;
+}) {
   // const finalLotId = type === "b" ? lotId : id; //TODO: check lot type validation
   const finalLotId = lotId;
   const label = `View Lot #${finalLotId}`;
+  const id = batchStatus === undefined ? _id : lotIdMongo;
   return finalLotId ? (
-    <NextLink href={`/lot/show/${_id}`} passHref={true}>
+    <NextLink href={`/lot/show/${id}`} passHref={true}>
       <Link>
         <Tooltip label={label} aria-label={label}>
           <span>

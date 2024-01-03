85bc40b764981f5ca43c0b19d414871b3ae323ff
logan1x <sharmakhushal78@gmail.com>
2024-01-03 16:11:05 +0530

    Farmer API's for show and edit page

--- src/pages/farmer/show/[id].tsx
+++ src/pages/farmer/show/[id].tsx
@@ -1,11 +1,11 @@
 import FarmerShowPageComponent from "@components/pages/farmer-member/show";
-import { axGetFarmerById } from "@services/farmer.service";
+import { axGetFarmerByIdWithBatchAndFarmerProduce } from "@services/farmer.service";
 import React from "react";
 
 const FarmerShowPage = ({ farmer }) => <FarmerShowPageComponent show={farmer} />;
 
 FarmerShowPage.getInitialProps = async (ctx) => {
-  const { data } = await axGetFarmerById(ctx.query.id, ctx);
+  const { data } = await axGetFarmerByIdWithBatchAndFarmerProduce(ctx.query.id, ctx);
   return { farmer: data };
 };
 

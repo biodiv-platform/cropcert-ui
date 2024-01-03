f0001549b89b0ee6c426c87d06eee1d5e5e07ce7
logan1x <sharmakhushal78@gmail.com>
2024-01-03 16:15:12 +0530

    reaf: changed related to farmer data store schema

--- src/components/pages/farmer-member/list/data.tsx
+++ src/components/pages/farmer-member/list/data.tsx
@@ -11,61 +11,32 @@ export const batchColumns = [
   },
   {
     name: "Name",
-    selector: (row) => row["personalDetails"]["farmerName"],
+    selector: (row) => row["farmerName"],
     width: "280px",
   },
   {
     name: "Village",
-    selector: (row) => row["personalDetails"]["village"],
+    selector: (row) => row["village"],
     maxWidth: "260px",
     sortable: true,
   },
   {
     name: "Collection Center",
-    selector: (row) => row["personalDetails"]["cc"],
+    selector: (row) => row["cc"],
     maxWidth: "250px",
     sortable: true,
   },
   {
     name: "No. of Coffee Trees",
-    selector: (row) => row["farmDetails"]["noOfCoffeeTrees"],
+    selector: (row) => row["noOfCoffeeTrees"],
     maxWidth: "210px",
     sortable: true,
     right: true,
   },
   {
     name: "Land Acreage",
-    selector: (row) => row["farmDetails"]["landAcreage"],
+    selector: (row) => row["landAcreage"],
     maxWidth: "210px",
-    sortable: true,
-    right: true,
-  },
-];
-
-export const lotCreateModalCols = [
-  {
-    name: "#",
-    selector: (row) => row["farmerProduceId"],
-    width: "100px",
-    cell: (row) => `FP-${row.farmerProduceId}`,
-  },
-  {
-    name: "Name",
-    selector: (row) => row["farmerName"],
-    width: "200px",
-  },
-  {
-    name: "Quantity",
-    selector: (row) => row["quantity"],
-    sortable: true,
-    right: true,
-  },
-];
-
-export const lotCreateModalColsExtra = [
-  {
-    name: "Perchment Quantity",
-    selector: (row) => row["perchmentQuantity"],
     sortable: true,
     right: true,
   },

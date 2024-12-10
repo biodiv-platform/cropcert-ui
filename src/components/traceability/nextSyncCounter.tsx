af5ae78ecddb26e1fc60fc181b2fe5f5680a33ac
logan1x <sharmakhushal78@gmail.com>
2024-12-10 15:00:49 +0530

    reaf: padding for second in next sync counter

--- src/components/traceability/nextSyncCounter.tsx
+++ src/components/traceability/nextSyncCounter.tsx
@@ -26,7 +26,7 @@ export function NextSyncCounter({ syncIntervalHours }) {
       {Math.floor((time / 60) % 60)
         .toString()
         .padStart(2, "0")}
-      :{time % 60}
+      :{(time % 60).toString().padStart(2, "0")}
     </>
   );
 }

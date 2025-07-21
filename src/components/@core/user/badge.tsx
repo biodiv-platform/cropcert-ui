2777ab6ac8a63ff5eb7e27a1ea6397bd0358e1dd
logan1x <sharmakhushal78@gmail.com>
2025-07-21 13:25:17 +0530

    Change: badge icon for admin

--- src/components/@core/user/badge.tsx
+++ src/components/@core/user/badge.tsx
@@ -1,11 +1,11 @@
 import Tooltip from "@components/@core/tooltip";
-import VerifiedIcon from "@icons/verified";
 import React from "react";
+import { LuBadgeCheck } from "react-icons/lu";
 
 const Badge = ({ isAdmin }) =>
   isAdmin ? (
     <Tooltip showArrow={true} positioning={{ placement: "right" }} title="Administrator">
-      <VerifiedIcon color="green.500" />
+      <LuBadgeCheck color="green" />
     </Tooltip>
   ) : null;
 

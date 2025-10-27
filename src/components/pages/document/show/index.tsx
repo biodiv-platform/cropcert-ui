ab7b86ecfecdb0476c7661ce165460ddd1b969f9
Arunbala Balasubramani <arunbala.balasubramani@strandls.com>
2025-10-27 16:54:26 +0530

    refacotr: :recycle: refactored supported types for document

--- src/components/pages/document/show/index.tsx
+++ src/components/pages/document/show/index.tsx
@@ -5,7 +5,8 @@ import styled from "@emotion/styled";
 import { ShowDocument } from "@interfaces/document";
 import { axAddDocumentComment } from "@services/activity.service";
 import { RESOURCE_TYPE } from "@static/constants";
-import { getDocumentPath } from "@utils/media";
+import { ACCEPTED_MIME_TYPE } from "@static/document";
+import { getDocumentFilePath, getDocumentPath } from "@utils/media";
 import React from "react";
 
 import DocumentHeader from "./header";
@@ -28,14 +29,40 @@ interface DocumentShowProps {
 export default function DocumentShowComponent({ document }: DocumentShowProps) {
   const documentPath = document?.uFile?.path || document?.document?.externalUrl;
 
+  const getDocumentType = (mimeType) => {
+    if (mimeType?.includes(ACCEPTED_MIME_TYPE.VIDEO)) {
+      return ACCEPTED_MIME_TYPE.VIDEO;
+    }
+    if (mimeType?.includes(ACCEPTED_MIME_TYPE.PDF) || document?.document?.externalUrl) {
+      return ACCEPTED_MIME_TYPE.PDF;
+    }
+    return undefined;
+  };
+
+  const renderDocument = (fileExtension: string | undefined) => {
+    switch (fileExtension) {
+      case ACCEPTED_MIME_TYPE.VIDEO:
+        return (
+          <Box>
+            <video width="100%" controls>
+              <source src={getDocumentFilePath(documentPath || "")} />
+            </video>
+          </Box>
+        );
+      case ACCEPTED_MIME_TYPE.PDF:
+        return (
+          <DocumentIframe className="fadeInUp delay-2" src={getDocumentPath(documentPath || "")} />
+        );
+    }
+  };
+
   return (
     <Container my={6}>
       <DocumentHeader document={document} />
       <SimpleGrid columns={[1, 1, 3, 3]} gap={[1, 1, 4, 4]}>
         <Box gridColumn="1/3">
-          {documentPath && (
-            <DocumentIframe className="fadeInUp delay-2" src={getDocumentPath(documentPath)} />
-          )}
+          {renderDocument(getDocumentType(document?.uFile?.mimeType))}
+
           <DocumentInfo d={document} />
 
           <Activity
